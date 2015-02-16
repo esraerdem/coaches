e7cd125dad288f6e0820fcdb3c1dd6f1791b7900
jeanpierre <jeanpierre@carotte.info.unicaen.fr>
2015-02-16 17:15:01 +0100

    filling the successors in the policy

--- software/ros/catkin_ws/src/t42_multi_robot_planning/src/PRUplanner.cpp
+++ software/ros/catkin_ws/src/t42_multi_robot_planning/src/PRUplanner.cpp
@@ -157,11 +157,9 @@ void PRUplanner::plan() {
     else
       site = look4name(s);
     stateAction.state = "location( " + site + " )";
-    /*    if (s == TARGET)
-      stateAction.action = std::string(GOAL_INTERACT) + "( "+msg.final_state+" )";
-      else*/ 
     stateAction.action = *actions[act[s]].action + "( " + look4name(actions[act[s]].state) + " )";
-    
+
+    stateAction.successors.push_back(look4name(actions[act[s]].state));
     pol.push_back(stateAction);
     std::cout << "Location " << s << " (" << site << ", ";
     if (s==fixedSites.size())
