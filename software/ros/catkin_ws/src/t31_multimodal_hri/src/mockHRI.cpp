6321bf6a908dd641c56628e4ed83e9cd582236a0
jeanpierre <jeanpierre@carotte.info.unicaen.fr>
2015-03-13 15:46:27 +0100

    added timer to remove speech message on stage

--- software/ros/catkin_ws/src/t31_multimodal_hri/src/mockHRI.cpp
+++ software/ros/catkin_ws/src/t31_multimodal_hri/src/mockHRI.cpp
@@ -80,6 +80,8 @@ void MockHRI::hriGoalCallback(const shared::Goal::ConstPtr& msg)
     interactionTimer.start();
   } else if (msg->kind == GOAL_SPEECH) {
     msgOut.data = msg->param;
+    interactionTimer.setPeriod(ros::Duration(msgOut.data.length()/10.));
+    interactionTimer.start();
   }
   currentInteraction = msg->param;
   doSay(msgOut);
